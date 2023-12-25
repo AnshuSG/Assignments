590d361a52ac28dc9cb3220958ed5c2f270de3de
AnshuSG <gantianshu@gmail.com>
2023-12-25 07:55:03 +0530

    Added one more condition for leap year not by 100, or divisible by 400

--- LeapYear.java
+++ LeapYear.java
@@ -2,13 +2,14 @@ package src.Assignments;
 
 import java.util.Scanner;
 
+  // divisible by 4, not by 100, or divisible by 400
 public class LeapYear {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the year");
         int Year = sc.nextInt();
-        if(Year % 4==0)
+        if((Year % 4 == 0) && (Year % 100 != 0 || Year % 400 == 0))
         {
             System.out.println(" Year is Leap Year");
         }
